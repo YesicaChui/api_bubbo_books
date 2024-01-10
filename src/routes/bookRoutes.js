09feeea903b39ec92e32ab88343c9041dda1b336
YesicaChui <yesicachuic@gmail.com>
2024-01-10 06:49:13 -0500

    conexion con firebase usando firebase-admin

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const bookControllers = require("../controllers/bookControllers")
-router.get("/",bookControllers.getBooks)
+
+const {db}=require('../firebase')
+
+
+// router.get("/",bookControllers.getBooks)
+router.get("/",async(req,res)=>{
+  const querySnapshot = await db.collection('users').get()
+  console.log(querySnapshot.docs[0].data())
+  res.send("holaaaaaa firebase")
+})
 router.get("/:id",bookControllers.getBook)
 router.post("/",bookControllers.createBook)
 router.put("/:id",bookControllers.updateBook)
